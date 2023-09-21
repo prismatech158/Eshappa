c6e24caa75cce1a1e6ef3010977f73f5f6a9a6b9
Merlin9353 <eliyat2001@gmail.com>
2023-09-21 15:05:10 +0530

    main

---
+++ index.java
@@ -0,0 +1,8 @@
+public class Javaprogram {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c=a/b;
+        System.out.println("Addition of "+a+" and "+b+" = "+c);
+    }
+    }
